40615e8b440d69bda2009f18cfe5eae07eacb960
Ramon Amorim <ramonamorim33@gmail.com>
2014-04-24 19:59:35 -0300

    Ler Txt
    Ler Txt

--- teste/src/teste/helloWorld.java
+++ teste/src/teste/helloWorld.java
@@ -1,10 +1,21 @@
 package teste;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class helloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
+    public static void main(String[] args) throws IOException {
 
+	FileReader r = new FileReader("//Users/ramonamorim/Dropbox/teste.txt");
+
+	Scanner scan = new Scanner(r);
+	BufferedReader buff = new BufferedReader(r);
+	while (scan.hasNext()) {
+
+	    System.out.println(scan.next());
 	}
-
+    }
 }
